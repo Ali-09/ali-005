7529d293930ee5c957693f94a0688c2769c0bc13
Ali-09 <yuzet.ali@gmail.com>
2022-09-30 15:04:25 -0500

    feat: add isg in pages pokemon for name and id

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -98,28 +98,40 @@ const PokemonPage: NextPage<Props> = ({ pokemon }) => {
             </Grid.Container>
         </Layout>
     )
-}
+};
 
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
     const { data } = await pokeApi.get<PokemonListResponse>("/pokemon?limit=151");
-
+    
     return {
         paths: data.results.map(({ name }) => ({
             params: { name }
         })),
-        fallback: false
-    }
-}
+        //fallback: false
+        fallback: "blocking"
+    };
+};
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
 
     const { name } = params as { name: string };
+
+    const pokemon = await getPokemonInfo(name);
+
+    if(!pokemon){
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false
+            }
+        }
+    }
     
     return {
         props: {
-            pokemon: await getPokemonInfo(name)
+            pokemon
         }
     };
-}
+};
 
 export default PokemonPage;
